c47d5837ff054558bfdb4fd35781a7df99b8480c
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-11-04 12:03:15 +0100

    lecture 3: linear regression

--- examples/03_linear_regression_starter.py
+++ examples/03_linear_regression_starter.py
@@ -25,42 +25,47 @@ data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
 n_samples = sheet.nrows - 1
 
 # Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
-# Both have the type float32
-
+x = tf.placeholder(dtype=tf.float32, shape=(), name='x')
+y = tf.placeholder(dtype=tf.float32, shape=(), name='y')
 
 # Step 3: create weight and bias, initialized to 0
-# name your variables w and b
-
+w = tf.Variable(initial_value=0.0, name='w')
+b = tf.Variable(initial_value=0.0, name='b')
 
 # Step 4: predict Y (number of theft) from the number of fire
 # name your variable Y_predicted
-
+y_predicted = x * w + b
 
 # Step 5: use the square error as the loss function
-# name your variable loss
-
+#loss = tf.nn.l2_loss(y_predicted - y, name='loss')
+loss = utils.huber_loss(y, y_predicted)
 
 # Step 6: using gradient descent with learning rate of 0.01 to minimize loss
- 
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+
 # Phase 2: Train our model
 with tf.Session() as sess:
-	# Step 7: initialize the necessary variables, in this case, w and b
-	# TO - DO	
+  # Step 7: initialize the necessary variables, in this case, w and b
+  sess.run(tf.global_variables_initializer())
+  writer = tf.summary.FileWriter('/tmp/linear-reg', sess.graph)
 
+# Step 8: train the model
+  for i in range(100):  # run 100 epochs
+    total_loss = 0
+    for train_x, train_y in data:
+      # Session runs optimizer to minimize loss and fetch the value of loss. Name the received value as l
+      _, l = sess.run([optimizer, loss], feed_dict={x: train_x, y: train_y})
 
-	# Step 8: train the model
-	for i in range(50): # run 100 epochs
-		total_loss = 0
-		for x, y in data:
-			# Session runs optimizer to minimize loss and fetch the value of loss. Name the received value as l
-			# TO DO: write sess.run()
+      total_loss += l
+    print("Epoch {0}: {1}".format(i, total_loss / n_samples))
 
-			total_loss += l
-		print("Epoch {0}: {1}".format(i, total_loss/n_samples))
-	
+  writer.close()
+
+  w_value, b_value = sess.run([w, b])
+
 # plot the results
-# X, Y = data.T[0], data.T[1]
-# plt.plot(X, Y, 'bo', label='Real data')
-# plt.plot(X, X * w + b, 'r', label='Predicted data')
-# plt.legend()
-# plt.show()
+X, Y = data.T[0], data.T[1]
+plt.plot(X, Y, 'bo', label='Real data')
+plt.plot(X, X * w_value + b_value, 'r', label='Predicted data')
+plt.legend()
+plt.show()
